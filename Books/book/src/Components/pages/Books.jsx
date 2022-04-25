b6af5233a20caa85d7085e871f2203be42e919d4
Sachin Kumar Keshri <helpmeskk@gmail.com>
2022-04-25 17:57:45 +0530

    Sachin

--- Books/book/src/Components/pages/Books.jsx
+++ Books/book/src/Components/pages/Books.jsx
@@ -5,23 +5,50 @@ import { BookCard } from "./BookCard";
 import styled, { css } from "styled-components";
 
 export const Grid = styled.div`
- add required style here
+   display:grid;
+   grid-template-columns:200px 200px 200px;
+   margin-left:300px;
+   gap:20px;
 `;
 
 const Books = () => {
-  const [data, setData] = useState([]);
+  const [data1, setData] = useState([]);
   useEffect(() => {
+     const getData = async()=>{
+       try {
+        let res = await fetch(` http://localhost:8000/books`);
+        let data = await res.json();
+        setData(data)
+       }
+       catch (e){
+           console.log(e)
+       }
+       
+     }
+     getData()
     // make a GET request to http://localhost:8080/books to get all the books data
   }, []);
-
+console.log(data1)
   return (
     <>
       <h1>Books</h1>
-      <Grid data-testid="books-container">
+      
+        {data1.map(item=>{
+           return (
+            <Grid data-testid="books-container">
+                
+                   <img src ={item.thumbnailUrl}/>
+                   <h4>{item.title}</h4>
+                   <p>{item.isbn}</p>
+                
+            </Grid>
+                
+           )
+        })}
         {/* {!!data && 
           // map thorugh the data and use <BookCard/> component to display each book
           } */}
-      </Grid>
+     
     </>
   );
 };
